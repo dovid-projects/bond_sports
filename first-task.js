2d7c302ba3de4af9aabbd3102c685f3169f79558
dovid projects <dovid.projects@gmail.com>
2023-03-28 21:09:36 +0300

    first task - change to num

--- first-task.js
+++ first-task.js
@@ -1,6 +1,6 @@
 /**
  *
- * Your task is to make a function that can take any ASCII  as
+ * Your task is to make a function that can take any ASCII number as
  * an argument and return it with its chars in descending order.
  *
  * Essentially, rearrange the chars to create the highest possible number.
@@ -9,13 +9,13 @@
  *
  */
 function sortTheHighestNum(asciiNum) {
-    var charArray = asciiNum.split("");
+    var charArray = asciiNum.toString().split("");
     charArray.sort(function (a, b) { return b.charCodeAt(0) - a.charCodeAt(0); });
     return charArray.join("");
 }
-var a = "531";
-var b = "145263";
-var c = "123456789";
+var a = 531;
+var b = 145263;
+var c = 123456789;
 console.log(sortTheHighestNum(a)); // result: 531
 console.log(sortTheHighestNum(b)); // result: 654321
 console.log(sortTheHighestNum(c)); // result: 987654321
--- first-task.ts
+++ first-task.ts
@@ -1,6 +1,6 @@
 /**
  * 
- * Your task is to make a function that can take any ASCII  as 
+ * Your task is to make a function that can take any ASCII number as 
  * an argument and return it with its chars in descending order. 
  * 
  * Essentially, rearrange the chars to create the highest possible number.
@@ -9,15 +9,15 @@
  * 
  */
 
-function sortTheHighestNum(asciiNum: string): string {
-  const charArray: string[] = asciiNum.split("");
+function sortTheHighestNum(asciiNum: number): string {
+  const charArray: string[] = asciiNum.toString().split("");
   charArray.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0));
   return charArray.join("");
 }
 
-const a = "531";
-const b = "145263";
-const c = "123456789";
+const a = 531;
+const b = 145263;
+const c = 123456789;
 
 console.log(sortTheHighestNum(a)); // result: 531
 console.log(sortTheHighestNum(b)); // result: 654321
